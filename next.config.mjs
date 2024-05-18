5c9dd46b7ac24e1a9cbde45d009897b33f874f90
lohith1601 <91121348+lohith1601@users.noreply.github.com>
2024-05-18 22:38:33 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // Add any other custom configurations here
+};
 
 export default nextConfig;

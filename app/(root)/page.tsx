a77660c09ee4d01017c306abc7cac0bcea7d8d28
lohith1601 <lohithganapathy16@gmail.com>
2024-05-20 14:35:10 +0530

    added1.3

--- app/(root)/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/(root)/page.tsx
-export default function HomePage() {
-  return (
-    <div className="p-4">
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main content area.</p>
-    </div>
-  );
-}
